d6591abe21e56b9126c3514abf911c93aefb13d5
Ziya Barutcu <128179749+ziyabarutcu@users.noreply.github.com>
2023-05-31 22:41:17 +0300

    veriler kütüphanesi oluşturuldu ve bunlar dataframe'e yazdırıldı

--- main.py
+++ main.py
@@ -82,3 +82,23 @@ i13.__str__()
 kullanici_bilgisi()
 i14=BeyazYaka("321123","isim14","soyad14","32","erkek","İspanyol",statu,tecrube,maas)
 i14.__str__()
+
+
+
+veri={
+    "Statü":["çalışan","çalışan","çalışan","mavi yakalı","mavi yakalı","mavi yakalı","beyaz yakalı","beyaz yakalı","beyaz yakalı"],
+    "TC No":[i6.get_tc_no(),i7.get_tc_no(),i8.get_tc_no(),i9.get_tc_no(),i10.get_tc_no(),i11.get_tc_no(),i12.get_tc_no(),i13.get_tc_no(),i14.get_tc_no()],
+    "Ad":[i6.get_ad(),i7.get_ad(),i8.get_ad(),i9.get_ad(),i10.get_ad(),i11.get_ad(),i12.get_ad(),i13.get_ad(),i14.get_ad()],
+    "Soyad":[i6.get_soyad(),i7.get_soyad(),i8.get_soyad(),i9.get_soyad(),i10.get_soyad(),i11.get_soyad(),i12.get_soyad(),i13.get_soyad(),i14.get_soyad()],
+    "Yaş":[i6.get_yas(),i7.get_yas(),i8.get_yas(),i9.get_yas(),i10.get_yas(),i11.get_yas(),i12.get_yas(),i13.get_yas(),i14.get_yas()],
+    "Cinsiyet":[i6.get_cinsiyet(),i7.get_cinsiyet(),i8.get_cinsiyet(),i9.get_cinsiyet(),i10.get_cinsiyet(),i11.get_cinsiyet(),i12.get_cinsiyet(),i13.get_cinsiyet(),i14.get_cinsiyet()],
+    "Uyruk":[i6.get_uyruk(),i7.get_uyruk(),i8.get_uyruk(),i9.get_uyruk(),i10.get_uyruk(),i11.get_uyruk(),i12.get_uyruk(),i13.get_uyruk(),i14.get_uyruk()],
+    "Sektör":[i6.get_statu(),i7.get_statu(),i8.get_statu(),i9.get_statu(),i10.get_statu(),i11.get_statu(),i12.get_statu(),i13.get_statu(),i14.get_statu()],
+    "Tecrübe":[i6.get_tecrube()/12,i7.get_tecrube()/12,i8.get_tecrube()/12,i9.get_tecrube()/12,i10.get_tecrube()/12,i11.get_tecrube()/12,i12.get_tecrube()/12,i13.get_tecrube()/12,i14.get_tecrube()/12],
+    "Maaş":[i6.get_maas(),i7.get_maas(),i8.get_maas(),i9.get_maas(),i10.get_maas(),i11.get_maas(),i12.get_maas(),i13.get_maas(),i14.get_maas()],
+    "Yıpranma Payı":["0","0","0",i9.get_yipranma_payi(),i10.get_yipranma_payi(),i11.get_yipranma_payi(),"0","0","0"],
+    "Teşfik Primi":["0","0","0","0","0","0",i12.get_tesfik_primi(),i13.get_tesfik_primi(),i14.get_tesfik_primi()],
+    "Yeni Maaş":[i6.get_yeni_maas(),i7.get_yeni_maas(),i8.get_yeni_maas(),i9.get_yeni_maas(),i10.get_yeni_maas(),i11.get_yeni_maas(),i12.get_yeni_maas(),i13.get_yeni_maas(),i14.get_yeni_maas()]
+}
+df=pd.DataFrame(veri)
+df.to_excel("output.xlsx")
